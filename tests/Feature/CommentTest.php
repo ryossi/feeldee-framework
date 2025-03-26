4d133beb6392a84079618564479838ea1476c259
Ryo Sskai <r.sakai@aifulmf.com>
2025-03-26 11:04:43 +0900

    機能テスト周りの整備

---
+++ tests/Feature/CommentTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use Feeldee\Framework\Models\Profile;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+/**
+ * コメントの用語を担保するための機能テストです。
+ * 
+ * @link https://github.com/ryossi/feeldee-framework/wiki/コメント#コメント
+ */
+class CommentTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * コメント日時
+     * 
+     * @link https://github.com/ryossi/feeldee-framework/wiki/コメント#コメント日時
+     */
+    public function test_コメント日時_コメント日時指定あり()
+    {
+
+        // 準備
+        $profile = Profile::factory()->create();
+    }
+
+    /**
+     * コメント日時
+     * 
+     * @link https://github.com/ryossi/feeldee-framework/wiki/コメント#コメント日時
+     */
+    public function test_コメント日時_コメント日時指定なし() {}
+
+    /**
+     * 公開フラグ
+     * 
+     * @link https://github.com/ryossi/feeldee-framework/wiki/コメント#公開フラグ
+     */
+    public function test_コメント公開() {}
+}
