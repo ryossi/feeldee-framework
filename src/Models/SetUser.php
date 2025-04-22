ff98d79890055d152c5952721e3ec668735c8a3a
Ryo Sskai <r.sakai@aifulmf.com>
2025-04-22 10:15:09 +0900

    SetUserObserver廃止

--- src/Models/SetUser.php
+++ src/Models/SetUser.php
@@ -2,12 +2,22 @@
 
 namespace Feeldee\Framework\Models;
 
-use Feeldee\Framework\Observers\SetUserObserver;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
 
 trait SetUser
 {
     public static function bootSetUser()
     {
-        self::observe(SetUserObserver::class);
+        static::creating(function (Model $model) {
+            $id = Auth::id();
+            $model->created_by = $id;
+            $model->updated_by = $id;
+        });
+
+        static::updating(function (Model $model) {
+            $model->updated_by = Auth::id();
+        });
     }
 }
